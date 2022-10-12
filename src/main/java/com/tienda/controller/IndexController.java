8ec53c0904acb157ecaca812a1365b9b0ebc0ef8
monica-monterrosa <monimonte13@gmail.com>
2022-10-12 16:58:15 -0600

    Semana 4 Parte II, proyecto inicial del curso Viernes IIIC 2022

--- src/main/java/com/tienda/controller/IndexController.java
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -1,25 +1,35 @@
 package com.tienda.controller;
-
+import com.tienda.dao.ClienteDao;
 import com.tienda.domain.Cliente;
 import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+     
+    //Provoca que el objeto se cree si no existe o se use el que existe, no hace más de 1 objeto
+    //Se conoce como inyección de dependencias
+    @Autowired
+    private ClienteDao clienteDao;
+    
+    
     @GetMapping("/")
     public String inicio(Model model){
         var texto="Estamos en semana 4";
         model.addAttribute("saludo",texto);
         
-        Cliente cliente1=new Cliente("Pedro","Jimenez Retana", "pjimenez@gmail.com","8989-9898");
+        /*Cliente cliente1=new Cliente("Pedro","Jimenez Retana", "pjimenez@gmail.com","8989-9898");
         
         Cliente cliente2=new Cliente("Juan","Retana Jimenez", "jretana@gmail.com","9898-8989");
         
         Cliente cliente3=new Cliente("Juancho","Retana Jimenez", "jretana@gmail.com","9898-8989");
         
-        var clientes=Arrays.asList(cliente1, cliente2,cliente3);
+        var clientes=Arrays.asList(cliente1, cliente2,cliente3);*/
+        
+        var clientes = clienteDao.findAll();
         
         model.addAttribute("clientes",clientes);
         return "index";
--- src/main/java/com/tienda/domain/Cliente.java
+++ src/main/java/com/tienda/domain/Cliente.java
@@ -1,11 +1,20 @@
 
 package com.tienda.domain;
-
+import java.io.Serializable;
+import javax.persistence.*;
 import lombok.Data;
 
 @Data
-public class Cliente {
-   
+@Entity
+@Table(name="cliente")
+
+public class Cliente implements Serializable{
+    private static final long serialVersionUID=1L;
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long idCliente;
+    
     private String nombre;
     private String apellidos;
     private String correo;
@@ -18,4 +27,3 @@ public Cliente(String nombre, String apellidos, String correo, String telefono){
     this.telefono = telefono;
 }    
 }
-
