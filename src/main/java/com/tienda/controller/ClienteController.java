41f4f940e3602080d4b4b4eb1cd02eb754804982
monica-monterrosa <monimonte13@gmail.com>
2022-11-28 11:43:23 -0600

    Semana 11, proyecto inicial del curso Viernes IIIC 2022

--- src/main/java/com/tienda/controller/ClienteController.java
+++ src/main/java/com/tienda/controller/ClienteController.java
@@ -1,4 +1,5 @@
 package com.tienda.controller;
+
 import com.tienda.domain.Cliente;
 import com.tienda.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,39 +10,47 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ClienteController {
-     
+
     //Provoca que el objeto se cree si no existe o se use el que existe, no hace más de 1 objeto
     //Se conoce como inyección de dependencias
     @Autowired
     private ClienteService clienteService;
-    
+
     @GetMapping("/cliente/listado")
-    public String inicio(Model model){
+    public String inicio(Model model) {
         var clientes = clienteService.getClientes();
-        model.addAttribute("clientes",clientes);
+        model.addAttribute("clientes", clientes);
+
+        var limiteTotal = 0;
+        for (var c : clientes) {
+            limiteTotal += c.credito.limite;
+        }
+        model.addAttribute("limiteTotal", limiteTotal);
+        model.addAttribute("totalClientes", clientes.size());
+
         return "/cliente/listado";
     }
-    
+
     @GetMapping("/cliente/nuevo")
-    public String clienteNuevo(Cliente cliente){
+    public String clienteNuevo(Cliente cliente) {
         return "/cliente/modificar";
     }
-    
-     @PostMapping("/cliente/guardar")
-    public String clienteGuardar(Cliente cliente){
+
+    @PostMapping("/cliente/guardar")
+    public String clienteGuardar(Cliente cliente) {
         clienteService.save(cliente);
         return "redirect:/cliente/listado";
     }
-    
-    @GetMapping("/cliente/actualiza/{idCliente}")
-    public String clienteActualiza(Cliente cliente, Model model){
-        cliente=clienteService.getCliente(cliente);
-        model.addAttribute("cliente",cliente);
+
+    @GetMapping("/cliente/modificar/{idCliente}")
+    public String clienteActualiza(Cliente cliente, Model model) {
+        cliente = clienteService.getCliente(cliente);
+        model.addAttribute("cliente", cliente);
         return "/cliente/modificar";
     }
-    
-    @GetMapping("/cliente/elimina/{idCliente}")
-    public String clienteElimina(Cliente cliente){
+
+    @GetMapping("/cliente/eliminar/{idCliente}")
+    public String clienteElimina(Cliente cliente) {
         clienteService.delete(cliente);
         return "redirect:/cliente/listado";
     }
--- src/main/java/com/tienda/domain/Cliente.java
+++ src/main/java/com/tienda/domain/Cliente.java
@@ -23,7 +23,7 @@ public class Cliente implements Serializable{
     private String telefono;
     
     @JoinColumn(name="id_credito", referencedColumnName="id_credito" )
-    private Credito credito;
+    public Credito credito;
 
     public Cliente() {
     }
